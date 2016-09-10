fe3ec74f085464ce38de0c0b3fc8b56280f1b439
justin <justinyangatang@gmail.com>
2016-09-09 20:26:58 -0400

    add files

---
+++ popup.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('checkPage');
+  checkPageButton.addEventListener('click', function() {
+
+    chrome.tabs.getSelected(null, function(tab) {
+      d = document;
+      var f = d.createElement('form');
+      f.action = 'http://www.ceca.uwaterloo.ca/students/sessions.php';
+      f.method = 'post';
+      var i = d.createElement('input');
+      i.type = 'hidden';
+      i.name = 'url';
+      i.value = tab.url;
+      f.appendChild(i);
+      d.body.appendChild(f);
+      f.submit();
+    });
+  }, false);
+}, false);
